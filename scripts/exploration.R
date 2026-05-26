00d2195a9f4b1b0a0a9682c9756e7c26c6e67a3a
MEYNEN Frederik <frederik.meynen@uhasselt.be>
2026-05-26 21:14:47 +0200

    Update exploration.R

--- scripts/exploration.R
+++ scripts/exploration.R
@@ -1,1 +1,43 @@
 rm(list = ls())
+
+library(ggplot2)
+library(ordinal)
+
+data_loc   <- "data/processed"
+files_list <- list.files(data_loc)
+data_list  <- lapply(files_list, function(f) {
+  readRDS(paste0(data_loc, "/", f))
+})
+names(data_list) <- files_list
+rm(data_loc, files_list)
+
+
+lapply(seq_along(data_list), function(i){
+  var <- names(data_list)[i]
+  data <- data_list[[i]]
+  names(data)[2] <- "Baseline"
+  names(data)[3] <- "Treatment"
+  
+  ggplot(data = data, aes(x = Baseline, y = Treatment, color = SPID)) +
+    geom_point() +
+    ggtitle(var)
+})
+
+names(data_list)
+
+lapply(seq(from = 1, to = 21), function(i){
+  var <- names(data_list)[i]
+  data <- data_list[[i]]
+  model <- lm(Treatment ~ SPID + as.numeric(Baseline) + age + sex, data = data)
+  cat(var)
+  plot(model)
+})
+
+?ks.test()
+
+lapply(seq(from = 22, to = 24), function(i){
+  var <- names(data_list)[i]
+  data <- data_list[[i]]
+  model <- clm(Treatment ~ SPID + Baseline + age + sex, data = data)
+  plot(model)
+})
